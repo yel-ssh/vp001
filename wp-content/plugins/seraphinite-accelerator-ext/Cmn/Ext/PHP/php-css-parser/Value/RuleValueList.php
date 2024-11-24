ea10690f696948d4c64a7413a5da20bcf891b93e
yel-ssh <victorvssh@gmail.com>
2024-11-24 11:40:53 -0300

    update

--- wp-content/plugins/seraphinite-accelerator-ext/Cmn/Ext/PHP/php-css-parser/Value/RuleValueList.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace seraph_accel\Sabberworm\CSS\Value;
-
-class RuleValueList extends ValueList {
-	public function __construct($sSeparator = ',', $iPos = 0) {
-		parent::__construct(array(), $sSeparator, $iPos);
-	}
-}
