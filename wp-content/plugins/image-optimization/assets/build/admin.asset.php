ff9e38a99e059df2f5e3f8db25a25418e4f361b7
yel-ssh <victorvssh@gmail.com>
2024-11-23 21:43:35 -0300

    update

---
+++ wp-content/plugins/image-optimization/assets/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '854988ee84bc360edc11');
---
+++ wp-content/plugins/image-optimization/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/image-optimizer',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b2cd41d9a5c9b460d3a12c199135e87035e014cd',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/image-optimizer' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b2cd41d9a5c9b460d3a12c199135e87035e014cd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'woocommerce/action-scheduler' => array(
+            'pretty_version' => '3.9.0',
+            'version' => '3.9.0.0',
+            'reference' => '90b98e6fe97d455679b1d288f050cad8f6f79771',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../woocommerce/action-scheduler',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
