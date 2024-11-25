60984f464b082dd4ef4259b5c4d611e31fc3a83a
yel-ssh <victorvssh@gmail.com>
2024-11-24 21:35:05 -0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -100,4 +100,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
-$_SERVER['HTTPS'] = 'on';
+$_SERVER['HTTPS'] == 'on';
